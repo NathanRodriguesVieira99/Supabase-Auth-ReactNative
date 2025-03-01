6c0c1bf06ba55e252cb73f77ba1ed7a5aaae1be7
Nathan Rodrigues Vieira <116213876+NathanRodriguesVieira99@users.noreply.github.com>
2025-03-01 17:04:50 -0300

    feat: add styles for loading component

---
+++ src/app/styles.ts
@@ -0,0 +1,13 @@
+import { StyleSheet } from 'react-native';
+
+import colors from '../constants/colors/colors';
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 34,
+    backgroundColor: colors.white,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
