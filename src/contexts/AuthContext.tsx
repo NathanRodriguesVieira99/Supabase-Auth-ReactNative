586958a8aab350a5a6a14ed8aac78f5f0b1b49a6
Nathan Rodrigues Vieira <116213876+NathanRodriguesVieira99@users.noreply.github.com>
2025-03-01 17:03:41 -0300

    feat: add AuthContext

---
+++ src/contexts/AuthContext.tsx
@@ -0,0 +1,21 @@
+import type { User } from '@supabase/supabase-js';
+import { createContext, useContext, useState } from 'react';
+
+interface AuthContextProps {
+  user: User | null;
+  setAuth: (authUser: User | null) => void;
+}
+
+const AuthContext = createContext({} as AuthContextProps);
+
+export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
+  const [user, setUser] = useState<User | null>(null);
+
+  const setAuth = (authUser: User | null) => {
+    setUser(authUser);
+  };
+
+  return <AuthContext.Provider value={{ user, setAuth }}>{children}</AuthContext.Provider>;
+};
+
+export const useAuth = () => useContext(AuthContext);
