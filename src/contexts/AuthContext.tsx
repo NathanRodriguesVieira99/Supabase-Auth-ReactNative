d29b7984c7d6ad6b47fa8604d94e627bd123ad15
Nathan Rodrigues Vieira <116213876+NathanRodriguesVieira99@users.noreply.github.com>
2025-03-02 22:57:13 -0300

    feat: add comments to explain AuthContext

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -2,20 +2,22 @@ import type { User } from '@supabase/supabase-js';
 import { createContext, useContext, useState } from 'react';
 
 interface AuthContextProps {
-  user: User | null;
-  setAuth: (authUser: User | null) => void;
+    user: User | null;
+    setAuth: (authUser: User | null) => void;
 }
 
+// cria um contexto para verificar se está logado ou não
 const AuthContext = createContext({} as AuthContextProps);
 
 export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
-  const [user, setUser] = useState<User | null>(null);
+    const [user, setUser] = useState<User | null>(null);
 
-  const setAuth = (authUser: User | null) => {
-    setUser(authUser);
-  };
+    const setAuth = (authUser: User | null) => {
+        setUser(authUser);
+    };
 
-  return <AuthContext.Provider value={{ user, setAuth }}>{children}</AuthContext.Provider>;
+    return <AuthContext.Provider value={{ user, setAuth }}>{children}</AuthContext.Provider>;
 };
 
+// custom hook desse context
 export const useAuth = () => useContext(AuthContext);
